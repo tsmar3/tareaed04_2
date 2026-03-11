3a72d62f19f04fe7bb048d82502c706a8b79ce62
MARIA <MARIA@LAPTOP-T0VQDG70>
2026-03-11 12:37:08 +0100

    Versión inicial sin modificar

---
+++ JavaApplication10/src/javaapplication10/Pruebas.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication10;
+
+public class Pruebas {
+
+    public static int diasEnMesArray(int mes, int anio) {
+        int[] diasPorMes = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int dias = 0;
+
+        if (mes > 0 && mes < 13) {
+
+            dias = diasPorMes[mes];
+
+            if (mes == 2 && esBisiesto(anio)) {
+                dias++;
+            }
+        }
+        return dias;
+    }
+
+    public static boolean esBisiesto(int anio) {
+        return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+    }
+
+    public static double kmhAms(double kmh) {
+        return kmh / 3.6;
+    }
+
+    public static double gradosARadianes(double grados) {
+        return grados * Math.PI / 180;
+    }
+
+    public static double gradosARadianes(int grados, int minutos, int segundos) {
+        return gradosARadianes(grados + (minutos / 60.0) + (segundos / 3600.0));
+    }
+
+    public static double celsiusAFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitACelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static double areaCirculo(double radio) {
+        return Math.PI * alCuadrado(radio);
+    }
+
+    public static double longitudCircunferencia(double radio) {
+        return 2 * Math.PI * radio;
+    }
+
+    public static double alCuadrado(double num) {
+        return Math.pow(num, 2.0);
+    }
+
+    public static double hip(double c1, double catetoB) {
+        return Math.sqrt(alCuadrado(c1) + alCuadrado(catetoB));
+    }
+
+    public static double cat(double hipotenusa, double catetoB) {
+        return Math.sqrt(alCuadrado(hipotenusa) - alCuadrado(catetoB));
+    }
+
+    public static double mphAKmh(double mph) {
+        return mph * 1.60934;
+    }
+
+    public static double kmhAMph(double kmh) {
+        return kmh / 1.60934;
+    }
+
+}
