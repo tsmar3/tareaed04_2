270d42b70c1a611ac12bd2e1f76faf481237a8e0
MARIA <MARIA@LAPTOP-T0VQDG70>
2026-03-14 09:30:29 +0100

    Borrado seguro del método diasEnMesArray

--- JavaApplication10/src/tarea4/Apellido1NombreED04.java
+++ JavaApplication10/src/tarea4/Apellido1NombreED04.java
@@ -7,7 +7,6 @@ package tarea4;
 public class Apellido1NombreED04 {
 
     public static void main(String[] args) {
-        System.out.println("Febrero de 2026 tiene " + Utilidades.diasEnMesArray(2, 2026) + " días");
         System.out.println("60mph son " + Utilidades.kmhAms(Utilidades.mphAKmh(60)) + " metros por segundo");
         System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
                 + Utilidades.hipotenusa(3, 4) + "cm");
--- JavaApplication10/src/tarea4/Utilidades.java
+++ JavaApplication10/src/tarea4/Utilidades.java
@@ -6,21 +6,6 @@ package tarea4;
 
 public class Utilidades {
 
-    public static int diasEnMesArray(int mes, int anio) {
-        int[] diasPorMes = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        int dias = 0;
-
-        if (mes > 0 && mes < 13) {
-
-            dias = diasPorMes[mes];
-
-            if (mes == 2 && esBisiesto(anio)) {
-                dias++;
-            }
-        }
-        return dias;
-    }
-
     public static boolean esBisiesto(int anio) {
         return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
     }
